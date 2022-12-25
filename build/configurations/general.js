ee02381a691d8957d5695bafe70071b1f1771227
Marcuth <thiago.playgames15@gmail.com>
2022-12-25 00:24:33 +0000

    dadasd

--- build/configurations/general.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.general = void 0;
-exports.general = {
-    botName: "Spider Lite",
-    prefix: "#",
-    prefixEmoji: "🤖",
-};
